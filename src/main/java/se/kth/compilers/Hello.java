97f98ad31f0275bc4b0b75b64032df2b8c6d08a4
vishnuvardhana-rmr <vishnuvardhana.rmr@gmail.com>
2025-02-26 12:44:06 +0530

    Names.java

--- src/main/java/se/kth/compilers/Hello.java
+++ src/main/java/se/kth/compilers/Hello.java
@@ -2,10 +2,8 @@ package se.kth.compilers;
 
 public class Hello {
   public static void main(String[] args) {
-    if (args.length > 0) {
-      System.out.printf("Hello %s!", args[0]);
-      return;
-    }
-    System.out.print("Hello World!");
+   System.out.println("Vishnu");
+     System.out.println("Rahul");
+     System.out.println("Teja");
   }
 }
